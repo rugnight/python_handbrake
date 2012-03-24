608822debda58c2c14495852d622832f3f251e1c
rugnight <rugnight@gmail.com>
2012-03-24 23:45:15 +0900

    mod hb_test

--- handbrake/handbrake_test.py
+++ handbrake/handbrake_test.py
@@ -1,16 +1,51 @@
 # -*- coding: utf-8 -*-
 
+
+# ==================================================
+# 
+# 
+#   使い方
+#       convert_media SRC DST SETTING_FILE
+# 
+#   設定ファイル
+#       TITLE_VALID_DURATION    00:00:10
+#       CHAPTER_SPRIT           4
+# ==================================================
+
 import sys
 import commands
 import handbrake_media as HB
 
-#( all | chapter | setting_file )
+Usage = "Usage: python convert_media.py SRC DST_DIR SETTING_FILE"
 
 # 引数の処理
 argvs = sys.argv;
 argc  = len(argvs)
 
-TARGET_FILE = argvs[1]      # 変換対象ファイル名
+if argc < 4:
+    print Usage 
+
+TARGET_FILE  = argvs[1]      # 変換対象ファイル名
+OUTPUT_DIR   = argvs[2]      # 出力先ディレクトリ名
+SETTING_FILE = argvs[3]      # 設定ファイル
+
+# 映像メディアの情報を取得
+HB_RESEARCH_COMMAND = "HandBrakeCLI -i %s -t %s" % (TARGET_FILE, "0")
+research_cmd_return = commands.getstatusoutput(HB_RESEARCH_COMMAND)
+research_result = research_cmd_return[0]
+research_lines  = research_cmd_return[1]
+
+# なんか失敗
+if research_result == 0:
+    print "DVD情報の取得に失敗しました\n"
+    exit()
+
+# メディアの情報
+media = HB.HandBrakeMedia()
+media.set(research_lines)
+media.dump()
+
+exit()
 
 #HB_COMMAND = "HandBrakeCLI -i %s -t %s" % (TARGET_FILE, "0")
 HB_COMMAND = "ls"
@@ -19,19 +54,8 @@ results    = commands.getstatusoutput(HB_COMMAND)
 media_info = results[1]
 print media_info 
 
-exit()
-
-f = open("dvdinfo", "r")
 media = HB.HandBrakeMedia()
 media.set(f)
-media.dump()
 
 # メディアがどういう物か？
 analyse = HandBrakeMediaAnalyser()
-
-# メディアにあった変換ルールがあるか？
-
-# 
-
-f.close()
-
